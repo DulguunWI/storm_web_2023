32f16cceb6c8f5f618604775796a512c9b22b1cf
Storm-6 <storm-6@Admin-6-iMac.local>
2023-10-24 12:29:44 +0800

    lesson-day-19

---
+++ lesson-day-19/js/scripts.js
@@ -0,0 +1,43 @@
+console.log('Lesson day 19 - Js Control strucures exercises');
+
+
+let too = 10;
+let too2 = 20;
+let too3 = 30;
+
+
+let ner = 'Bold';
+let ner2 = 'Tulga';
+let ner3 = 'Naraa';
+
+
+let unen = true;
+let hudal = false;
+
+let isMongolian = true;
+let hasMongolianPassport = false;
+
+if (isMongolian) {
+    console.log('You are mongolian');
+}
+if (isMongolian) {
+    console.log('You are mongolian');
+}
+else if (isMongolian && hasMongolianPasspord) {
+    console.log('You are mongolian');
+}
+else {
+    console.log('You are not mongolian');
+}
+
+
+for (let i = 0 ; i < 10; i++) {
+    console.log(i);
+}
+
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
